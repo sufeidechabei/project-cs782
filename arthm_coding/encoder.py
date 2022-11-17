9817f49707f80445edefc9131f0543349798501e
Zhaoyuan Xu <zhaoyuan@snares-01.cs.wisc.edu>
2022-11-16 21:33:34 -0600

    [t] r=1 looks kinda promising. Still need to figureout a way to verify it

--- arthm_coding/encoder.py
+++ arthm_coding/encoder.py
@@ -11,19 +11,27 @@ def encode_basic(list_tokens, model_name=None):
     b = 1 << (r*l) # upper bound of the interval
     D = [] # ??? TODO
     #s = model.get_first_seed()
-    
+   
+    bb = b
+    print(str(a/bb)+","+str(b/bb))
     for token in list_tokens:
         a, b = util.Adjust(model_name, token, a, b)
+        print(str(a/bb)+","+str(b/bb))
         a, b, _, w = util.Rescale(D, w, a, b)
         #s = model.next_seed(#TODO
-
+        print(token+":", end="")
+        for n in D:
+            print(bin(n)[2:], end=" ")
+        print()
     return D, w 
 
 
 if __name__ == "__main__":
     tlist = ["I","really","eat","meat"]
     D,w = encode_basic(tlist)
-    print(D)
+    print("finally")
+    for n in D:
+        print(bin(n)[2:], end=" ")
     print(w)
 
 
--- arthm_coding/util.py
+++ arthm_coding/util.py
@@ -4,8 +4,8 @@ import random
 import math
 
 coding_parameters = {
-        "symbol_length":8,
-        "coding_length":4,
+        "symbol_length":1,
+        "coding_length":32,
         }
 
 
@@ -22,10 +22,13 @@ def Rescale(D, w, a, b, c=-1, C=None):
     l = coding_parameters["coding_length"]
     xor_val = 1 << (r*l-1)
     cmp_val = 1 << (r*(l-1))
+    ct = 0
     while(1):
+        ct = ct+1
         if w > 0 and double_floor_op(a, r*l, r*l-1) == double_floor_op(b, r*l, r*l-1):
             D[-w] = D[-w] + double_floor_op(a, r*l, r*l-1)
-            print("?? = "+str(double_floor_op(a, r*l, r*l-1)))
+            #print("?? = "+str(double_floor_op(a, r*l, r*l-1)))
+            print("w="+str(w))
             for i in range(1,w):
                 D[-i] = int(not D[-i])
             w = 0;
@@ -36,6 +39,9 @@ def Rescale(D, w, a, b, c=-1, C=None):
         if b - a < cmp_val:
             A = double_floor_op(a, r*l, r*(l-1))
             D.append(A)
+            for n in D:
+                print(bin(n)[2:], end=" ")
+            print()
             a = double_ceil_op(a, r*l, r)
             b = double_ceil_op(b, r*l, r)
             if A != double_floor_op(b, r*l, r*(l-1)):
@@ -45,11 +51,13 @@ def Rescale(D, w, a, b, c=-1, C=None):
                     c = c ^ xor_val
                 if C is not None and len(D) + l <= len(C):
                     c = c + C[len(D)+l]
-                else:
+                elif C is not None:
                     c = c+ dollar_sign(1 << r)
                     print("haha")
                 w = w+1
-        if b - a > cmp_val:
+        #print("a:"+str(a)+" b:"+str(b)+" w:"+str(w)+" cmp_val:"+str(cmp_val))
+
+        if b - a >= cmp_val or ct == 20:
             break
     return a, b, c, w
 
