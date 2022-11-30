f832405e1d0227664257d7eb42bfc58575f322d1
Matt Xu <xzy1998@cs.washington.edu>
2022-11-30 17:36:15 -0600

    [M] Added initial code to measure next's time(commented for now). Seems that getting the 3000 tokens from GPT-2 is the bottleneck

--- enhanced_version/gpt2_arthm_coding/gpt2model.py
+++ enhanced_version/gpt2_arthm_coding/gpt2model.py
@@ -4,6 +4,7 @@ from transformers import AutoModelForCausalLM, \
 # from torch import nn
 import numpy as np
 
+import time
 
 def my_normalizer(tensor_vals):
     z = np.array(tensor_vals)
@@ -22,6 +23,7 @@ class GPT2Model:
     
 
     def __init__(self, first_phrase="I think", toker = None, model = None):
+        self.parallel_mode = 0 # 0 for original next function. 1 for the parallel next function
         # GPT-2 and Pytorch params
         self.toker = toker
         self.model = model
@@ -33,19 +35,22 @@ class GPT2Model:
         # Initialize to get the first token.
         self.next()
 
-    def next(self,token=None):
+    def original_next(self,token=None):
         # update seed if needed
         if token is not None:
             self.current_seed = self.current_seed + token
         # start generation
+        #t0 = time.perf_counter()
         inpts = self.toker(self.current_seed, return_tensors="pt")
         with torch.no_grad():
             logits = self.model(**inpts).logits[:, -1, :]
         # logitis generated. now we can start calcualte the probabilities.
-        # firstly normalie the tensor vals
         tensor_vals = logits.topk(self.k_value).values.tolist()[0]
         # now get the tokens list
         token_indices = logits.topk(self.k_value).indices.tolist()[0]
+        #t1 = time.perf_counter()
+        #print(f"### Obtaining topK tokens took {t1 - t0:0.4f} s")
+        #t2 = time.perf_counter()
         tokens_list = []
         for index in token_indices:
             tokens_list.append(self.toker.decode(index))
@@ -64,13 +69,29 @@ class GPT2Model:
         # now the probabilities
         self.current_token_distro = []
         current_cumu = 0
+        #t3 = time.perf_counter()
+        #print(f"### Preparation for the distro generation took {t3 - t2:0.4f} s")
+        #t4 = time.perf_counter()
         for i in range(0, len(tokens_list)):
             token = tokens_list[i]
             cumu_freq = current_cumu / normalized_sum
             rela_freq = normalized_vals[i] / normalized_sum
             self.current_token_distro.append((token, cumu_freq, rela_freq))
             current_cumu += normalized_vals[i]
-        
+        #t5 = time.perf_counter()
+        #print(f"### Generation of the distro took {t5 - t4:0.4f} s\n");
+
+
+    def parallel_next(self, token=None):
+        print("WIP")
+
+
+    def next(self, token=None):
+        if self.parallel_mode == 0:
+            self.original_next(token=token)
+        else:
+            self.parallel_next(token=token)
+
 
 
     def GetToken(self, freq):
